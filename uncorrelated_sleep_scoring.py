6d690a6551466e8c00fbf03b615ff4dc55dd8769
Danut Matei <matei.danut.dm@gmail.com>
2022-04-10 20:58:23 +0000

    Cleaned up and commented code

--- uncorrelated_sleep_scoring.py
+++ uncorrelated_sleep_scoring.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-
 from sklearn.neural_network import MLPClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
@@ -15,29 +13,28 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 from numpy.random import default_rng
-rng = default_rng(42)
-
 from scipy.io import loadmat
-
-from pympler import asizeof
-
 import multiprocessing
 
+rng = default_rng(42)
+
+
 def load_data_spectrogram(eeg_files, idx):
+    # load data from files stoared as spectrograms
     with h5py.File(eeg_files[idx], 'r') as f:
         X = np.array(f['X'])
-#         y = np.array(f['y'])
         labels = np.array(f['label'])
 
     X = np.moveaxis(X, -1, 0)
     X = np.moveaxis(X, -1, 1)
-#     y = np.moveaxis(y, -1, 0)
 
+    # average the spectrogram on the time axis
     X_avg = X.mean(axis=1)
-#     y_true = np.where(y == 1)[1]
+
     # read the labels from the file, but make them 0 indexed
     y_true = (labels.T - 1).ravel()
     
+    # get rid of missing values
     valid = ~(np.isinf(X_avg) | np.isnan(X_avg))
     valid = np.all(valid, axis=1)
     X_avg, y_true = X_avg[valid], y_true[valid]
@@ -46,14 +43,15 @@ def load_data_spectrogram(eeg_files, idx):
 
 
 def load_data_signal(scans, idx):
+    # load data from files stored as signals
     scan_mat = loadmat(scans[idx])
     data, _, labels = scan_mat['data'], scan_mat['epoch'], scan_mat['labels']
 
     return data[..., 0], labels
 
 
-
 class DataSampler():
+    # class used for data sampling
     def __init__(self, data_type='spectrogram'):
         if data_type == 'signal':
             data_files = ['./raw_data/' + f for f in sorted(os.listdir('./raw_data'))]
@@ -64,17 +62,18 @@ class DataSampler():
         else:
             raise ValueError('data_type can only be "spectrogram" or "signal"!')
 
+        # load the whole data in memory from the start for faster sampling
         all_data = [load_data(data_files, idx) for idx in range(len(data_files))]
         self.X_full, self.y_full = zip(*all_data)
         self.data_count = len(data_files)
 
 
     def __call__(self, indices, n_samples_per_class=None, classes=None):
+        # sample data of specific patients
         X_avg = []
         y_true = []
 
         for idx in indices:
-            #X, y = load_data(data_files, idx)
             X, y = self.X_full[idx], self.y_full[idx]
 
             if n_samples_per_class is None:
@@ -92,64 +91,11 @@ class DataSampler():
 
             for cls in classes:
                 sample = np.where(y == cls)[0][:n_samples_per_class]
-#                 print('UNIQ', np.unique(y))
-#                 print('CLS', cls, sample)
                 X_avg.append(X[sample])
                 y_true.append(y[sample])
                 
-#             print(len(X_avg), X_avg[0].shape)
-
         return np.concatenate(tuple(X_avg)), np.concatenate(tuple(y_true))
 
-
-
-def make_data_sampler(data_type='spectrogram'):
-    if data_type == 'signal':
-        data_files = ['./raw_data/' + f for f in sorted(os.listdir('./raw_data'))]
-        load_data = load_data_signal
-    elif data_type == 'spectrogram':
-        data_files = ['./mat/' + f for f in sorted(os.listdir('./mat')) if f.endswith('eeg.mat')]
-        load_data = load_data_spectrogram
-    else:
-        raise ValueError('data_type can only be "spectrogram" or "signal"!')
-
-    all_data = [load_data(data_files, idx) for idx in range(len(data_files))]
-    X_full, y_full = zip(*all_data)
-
-    def sample_patient_data(indices, n_samples_per_class=None, classes=None):
-        X_avg = []
-        y_true = []
-
-#         for idx in tqdm(indices):
-        for idx in indices:
-            #X, y = load_data(data_files, idx)
-            X, y = X_full[idx], y_full[idx]
-
-            if n_samples_per_class is None:
-                X_avg.append(X)
-                y_true.append(y)
-                continue
-
-            p = rng.permutation(np.arange(len(y)))
-            # shuffle X and y
-            X, y = X[p, :], y[p]
-
-            # sample equally from each class, for each patient
-            if classes is None:
-                classes = list(range(5))
-
-            for cls in classes:
-                sample = np.where(y == cls)[0][:n_samples_per_class]
-#                 print('UNIQ', np.unique(y))
-#                 print('CLS', cls, sample)
-                X_avg.append(X[sample])
-                y_true.append(y[sample])
-                
-#             print(len(X_avg), X_avg[0].shape)
-
-        return np.concatenate(tuple(X_avg)), np.concatenate(tuple(y_true))
-    
-    return sample_patient_data, len(data_files)
 
 
 def train_pcas(sample_patient_data, X_train, n_samples_per_class):
@@ -161,7 +107,6 @@ def train_pcas(sample_patient_data, X_train, n_samples_per_class):
         model = PCA(n_components=7)
         model.fit(X)
         pcas.append(model)
-#         print(model.n_components_, sum(model.explained_variance_ratio_))
         
     return pcas
 
@@ -212,10 +157,7 @@ def fit_models(sample_patient_data, train_patients, n_samples_per_class, *, pca_
     return pcas, scaler, classifiers
 
 
-#def test_models(sample_patient_data, test_patients, pcas, scaler, classifiers):
-    #X_test, y_test = sample_patient_data(test_patients)
 def test_models(X_test, y_test, pcas, scaler, classifiers):
-
     X_pcas = pcas[0].transform(X_test)
 
     for pca in pcas[1:]:
@@ -228,8 +170,6 @@ def test_models(X_test, y_test, pcas, scaler, classifiers):
     for model in classifiers:
         y_pred = model.predict(X_test_std)
         
-#         print(model.__class__.__name__)
-#         print(classification_report(y_test, y_pred))
         d = classification_report(y_test, y_pred, output_dict=True)
         results.append((d['accuracy'], d['macro avg']['f1-score']))
         
@@ -242,15 +182,14 @@ def plot_results(n_samples, results):
     
     for i, (metric, metric_name) in enumerate([('acc', 'Accuracy'), ('f1', 'F1-score')]):
         test_res = list(zip(*results[metric]))
-        #print('test_res', test_res)
         
         axs[i].plot(n_samples, test_res[0], 'r-', label='knn')
         axs[i].plot(n_samples, test_res[1], 'g-', label='svm')
         axs[i].plot(n_samples, test_res[2], 'b-', label='mlp')
 
         if metric == 'acc':
+            # for the accuracy we also plot the "dummy" and Bayes Optimal classifiers
             axs[i].plot(n_samples, test_res[3], 'o-', label='majority_class')
-            #axs[i].plot(n_samples, test_res[4], 'o-', label='one_nn')
 
             one_nn_error = 1 - np.array(test_res[4])
             bayes_error = one_nn_error / 2
@@ -265,15 +204,16 @@ def plot_results(n_samples, results):
 
 
 def run_experiment():
-    rng = default_rng(42)
+    # function to call for training and testing the models,
+    # followed by plotting of results
     
-    #eeg_sampler, data_count = make_data_sampler()
     eeg_sampler = DataSampler()
     data_count = eeg_sampler.data_count
 
     TRAIN_SIZE = int(.8 * data_count)
     patient_permutation = rng.permutation(data_count)
 
+    # split the patients into train and test sets
     train_patients = patient_permutation[:TRAIN_SIZE]
     test_patients = patient_permutation[TRAIN_SIZE:]
     X_test, y_test = eeg_sampler(test_patients)
@@ -287,11 +227,13 @@ def run_experiment():
     
     n_samples = range(1, 90, 1)
 
+    # start a pool of processes to run the experiment in parallel
     pool = multiprocessing.Pool(20)
     args = [(eeg_sampler, train_patients, X_test, y_test, i) for i in n_samples]
     output = pool.starmap(train_test_classifiers_global, args)
     results['acc'], results['f1'] = zip(*output)
         
+    # plot the final accuracies and f1 scores for the trained models
     plot_results(n_samples, results)
 
 
